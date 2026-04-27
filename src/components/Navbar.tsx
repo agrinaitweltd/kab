ae7b0ce359db90a7db13ecc93887d6f39de154fa
agrinaitweltd <agrinaitwe@gmail.com>
2026-04-27 14:16:40 +0300

    Rebrcolours and logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <Link href="/" className="group z-50 transition-transform duration-300 hover:scale-105">
-            <div className="relative w-[155px] h-[89px]">
+            <div className="relative w-[355px] h-[189px]">
               <Image
                 src="/logo.png"
                 alt="Kabalega Society Foundation"
